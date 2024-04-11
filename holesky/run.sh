81b2c3edf315a5e415d20c9e744e78607eb8c74d
ChenYe <git@0xdf.com>
2024-04-11 17:57:51 +0800

    update log

--- holesky/run.sh
+++ holesky/run.sh
@@ -13,7 +13,10 @@ elif [ "$cmd" = "deposit" ]; then
 	_oprtool deposit "$@"
 elif [ "$cmd" = "operator" ]; then
 	. ./docker-compose-env.sh
-	docker compose up
+	docker compose up -d
+elif [ "$cmd" = "operator-log" ]; then
+	. ./docker-compose-env.sh
+	docker compose logs
 else
   echo "Invalid command"
 fi
