44a7714656ca70453313826ad56a8e4bfd794192
ChenYe <git@0xdf.com>
2024-06-11 11:14:06 +0800

    update mainnet version to 0.1.3

--- README.md
+++ README.md
@@ -8,7 +8,9 @@ Automata is building TEE Coprocessors on EigenLayer with Multi-Prover AVS. Decen
 
 * **v0.2.0**
   * [holesky](holesky/CHANGELOG.md)
-  * [mainnet](mainnet/CHANGELOG.md)
+
+* **v0.1.3**
+  * [mainnet](mainnet/README.md)
 
 ## Runbooks
 * [Operator - Holesky testnet](holesky/README.md)
--- mainnet/run.sh
+++ mainnet/run.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -e
 
-. $(dirname $0)/../utils/helper.sh
-cd $(dirname $0)
-_init_run
+. ./helper.sh
 
 cmd=$1
-if [[ "$cmd" == "" ]]; then
-	_available_cmd $0
-fi
 shift
 ecdsa_key=$1
 if [[ "$ecdsa_key" != "" ]]; then
@@ -16,19 +11,21 @@ fi
 
 if [ "$cmd" = "opt-in" ]; then
 	_require_ecdsa_key "$ecdsa_key"
+	. ./docker-compose-env.sh
 	_oprtool optin $ecdsa_key "$@"
 elif [ "$cmd" = "opt-out" ]; then
 	_require_ecdsa_key "$ecdsa_key"
+	. ./docker-compose-env.sh
 	_oprtool optout $ecdsa_key "$@"
 elif [ "$cmd" = "deposit" ]; then
 	_require_ecdsa_key "$ecdsa_key"
+	. ./docker-compose-env.sh
 	_oprtool deposit $ecdsa_key "$@"
 elif [ "$cmd" = "operator" ]; then
-	_operator_check
+	. ./docker-compose-env.sh
 	docker compose up -d
 elif [ "$cmd" = "operator-log" ]; then
 	docker compose logs
 else
-	echo "Invalid command"
-	_available_cmd $0
+  echo "Invalid command"
 fi
